64aac5e6ff90f7222a67a57c4fbf538c82023ce8
Mustafa Gaber <DrMustafaGaber@gmail.com>
2025-03-09 00:14:39 +0200

    lastnav

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,42 +1,56 @@
 import React from 'react';
+import './NavBar.css'
 import { NavLink } from 'react-router-dom';
-import { Dropdown } from 'react-bootstrap';
-import './NavBar.css';
 
 function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-crimson px-4 fixed-top">
-      <div className="container-fluid">
-        <a className="navbar-brand fw-bold" href="#">Aswan Services</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav mx-auto d-flex gap-4">
-            <li className="nav-item"><NavLink className="nav-link" to="/Home">Home</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" to="">Services</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" to="">Tourist places</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" to="">Housing</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" to="">About Us</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" to="">Voluntary</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" to="/Donation">Donation</NavLink></li>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark d-flex justify-content-around px-4">
+        <div>
+          <a className="navbar-brand" href="#">Aswan Services</a>
+        </div>
+        <div className="nav collapse navbar-collapse" id="navbarNav" >
+          <ul className="navbar-nav d-flex gap-5">
+            <li className="nav-item"><NavLink className="nav-link text-white" to="/Home">Home</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link text-white" to="/Services">Services</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link text-white" to="">Tourist places</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link text-white" to="">Housing</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link text-white" to="/AboutUs">About Us</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link text-white" to="/Community">Community</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link text-white" to="">Voluntary</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link text-white" to="/Donation">Donation</NavLink></li>
           </ul>
         </div>
-        <div className="d-flex align-items-center">
-          <Dropdown>
-            <Dropdown.Toggle variant="link" id="dropdown-profile" className="profile-container d-flex align-items-center text-decoration-none">
-              <span className="profile-icon">M</span>
-              <strong className="d-none d-lg-block text-white ms-2">Moursy</strong>
-            </Dropdown.Toggle>
-            <Dropdown.Menu className="dropdown-menu-end">
-              <Dropdown.Item href="#/action-1">Profile</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Settings</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Logout</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
+        <div className="d-flex align-items-center justify-content-atod">
+          <button className="navbar-toggler" style={{ marginRight: '15%' }} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <a href="#" className="d-flex align-items-center link-body-emphasis text-decoration-none ms-2" data-bs-toggle="dropdown" aria-expanded="false">
+            <span className="border p-1 me-2 border-success text-black-50" style={{
+              borderRadius: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#ddf9db',
+              width: '30px', height: '30px'
+            }}>M</span>
+            <strong className="d-none d-xl-block text-white" id="name">Moursy</strong>
+          </a>
+          <ul className="menu dropdown-menu text-small fw-bold shadow rounded-3 p-2 text-center" style={{
+            width: '220px', borderRadius: '50%', position: 'absolute', left: '84%', top: '54px'
+
+          }}>
+            <li>
+              <div className="d-flex justify-content-around align-items-center ">
+                <img src="https://fastly.picsum.photos/id/64/4326/2884.jpg?hmac=9_SzX666YRpR_fOyYStXpfSiJ_edO3ghlSRnH2w09Kg" alt="" width="32" height="32" className="rounded-circle me-2" />
+                <strong>Moursy</strong>
+                <span className="close">X</span>
+              </div>
+            </li>
+            <li><hr className="dropdown-divider" /></li>
+            <li><NavLink className="dropdown-item fw-bold" to="/Profile" >Account Setting</NavLink></li>
+            <li><a className="dropdown-item bg-danger fw-bold mt-2 px-5 w-25 justify-content-center m-auto d-flex rounded text-white" href="#">Sign out</a></li>
+          </ul>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
+
   );
 }
 
