449f78f2b150d6418c57550467178ed16037e755
MahmoudMoursy <mahmodmorsy301@gmail.com>
2025-03-16 11:44:45 +0200

    ubdate

--- src/authstorre.js
+++ src/authstorre.js
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { createUserWithEmailAndPassword} from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { auth } from "./firebaseconfig";
 
 export const useAuth = create((set) => ({
@@ -7,7 +7,20 @@ export const useAuth = create((set) => ({
     user: null,
     loading: false,
    
-      signUp: async (data) => {
+    login: async (data) => {
+        set({ loading: true });
+        try {
+            const res = await signInWithEmailAndPassword(auth, data.email, data.password);
+            const token = await res.user.getIdToken();
+            set({ user: res.user, token, loading: false });
+            return res.user; 
+        } catch (error) {
+            set({ loading: false });
+            throw error;      
+          }
+    },
+
+    signUp: async (data) => {
         set({ loading: true });
         try {
             const res = await createUserWithEmailAndPassword(auth, data.email, data.password);
@@ -20,5 +33,14 @@ export const useAuth = create((set) => ({
         }
     },
 
-   
+    logout: async () => {
+        set({ loading: true });
+        try {
+            await signOut(auth);
+            set({ user: null, token: "", loading: false });
+        } catch (error) {
+            set({ loading: false });
+            throw error; 
+        }
+    },
 }));
--- src/context/Authcontext.jsx
+++ src/context/Authcontext.jsx
@@ -1,61 +1,62 @@
-import React, { createContext, useState, useContext } from 'react';
-import { signInWithEmailAndPassword, signOut } from 'firebase/auth';
-import { auth } from '../firebaseconfig'; 
+// import React, { createContext, useState, useContext } from 'react';
+// import { signInWithEmailAndPassword, signOut } from 'firebase/auth';
+// import { auth } from '../firebaseconfig'; 
 
 
-const AuthContext = createContext();
+// const AuthContext = createContext();
 
 
-export function useAuth() {
-    return useContext(AuthContext);
-}
+// export function useAuth() {
+//     return useContext(AuthContext);
+// }
 
 
-export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState("");
+// export const AuthProvider = ({ children }) => {
+//     const [user, setUser] = useState(null);
+//     const [loading, setLoading] = useState(false);
+//     const [error, setError] = useState("");
 
-    const login = async (email, password) => {
-        setLoading(true);
-        setError("");
+//     const login = async (email, password) => {
+//         setLoading(true);
+//         setError("");
     
-        try {
-            const userCredential = await signInWithEmailAndPassword(auth, email, password);
-            const user = userCredential.user;
+//         try {
+//             const userCredential = await signInWithEmailAndPassword(auth, email, password);
+//             const user = userCredential.user;
 
-            setUser(user); 
-        } catch (err) {
-            setError("Invalid email or password. Please try again.");
-        } finally {
-            setLoading(false);
-        }
-    };
+//             setUser(user); 
+//         } catch (err) {
+//             setError("Invalid email or password. Please try again.");
+//         } finally {
+//             setLoading(false);
+//         }
+//     };
     
 
     
-    const logout = async () => {
-        try {
-            await signOut(auth);
-            setUser(null);
-        } catch (err) {
-            setError("Failed to log out. Please try again.");
-        }
-    };
+//     const logout = async () => {
+//         try {
+//             await signOut(auth);
+//             setUser(null);
+//         } catch (err) {
+//             setError("Failed to log out. Please try again.");
+//         }
+//     };
 
 
-    const value = {
-        user,
-        login,
-        logout,
-        loading,
-        error,
-        setError,
-    };
+//     const value = {
+//         user,
+//         login,
+//         logout,
+//         loading,
+//         error,
+//         setError,
+//         setLoading
+//     };
 
-    return (
-        <AuthContext.Provider value={value}>
-            {children}
-        </AuthContext.Provider>
-    );
-};
+//     return (
+//         <AuthContext.Provider value={value}>
+//             {children}
+//         </AuthContext.Provider>
+//     );
+// };
